e253cbd99644d5ece0f22e8ec607a6bcab1f1d0a
Marek Liška <software@marelis.cz>
2024-02-23 00:00:45 +0100

    Language switching and basic welcome

---
+++ lang/en/app.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in the app layout,
+    | which we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+];
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LangController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('lang/change', [LangController::class, 'change'])->name('change-lang');
